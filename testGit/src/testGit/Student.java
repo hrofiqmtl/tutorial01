06433c6c0a2d2c5aba5803519b305371e48601d6
Henri Rofiq <henri.rofiq@mail.mcgill.ca>
2020-09-11 20:37:47 -0400

    student attribute name added

--- testGit/src/testGit/Student.java
+++ testGit/src/testGit/Student.java
@@ -11,7 +11,7 @@ public class Student {
 		return name;
 	}
 	
-	public void setName (String name) {
-		this.name = name;
+	public void setName (String studenName) {
+		this.name = studenName;
 	}
 }
